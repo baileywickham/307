f876de26f35ce0a239d9840240ecf656d2a49f4f
baileywickham <baileywickham@gmail.com>
2020-04-10 22:58:39 -0700

    add scritps

---
+++ react-assignment1/build/precache-manifest.08c2cf7a15123e1b465d12e1542f9f4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588d8a93814f0e392d3dfbf5b19ac58a",
+    "url": "/307/part1/index.html"
+  },
+  {
+    "revision": "1281a5cfc21db1109e91",
+    "url": "/307/part1/static/css/main.bf286928.chunk.css"
+  },
+  {
+    "revision": "3e8c79a22fb78823fe0e",
+    "url": "/307/part1/static/js/2.de48b6e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/307/part1/static/js/2.de48b6e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1281a5cfc21db1109e91",
+    "url": "/307/part1/static/js/main.c2a997dd.chunk.js"
+  },
+  {
+    "revision": "69c8839e3d1b5258f915",
+    "url": "/307/part1/static/js/runtime-main.0e84900b.js"
+  }
+]);
